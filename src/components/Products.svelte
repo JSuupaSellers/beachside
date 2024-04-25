a4dfaa6fca5897d4e4dd4bd3a1badd915f26621e
Joshua Sellers <joshua.sellersdq@gmail.com>
2024-04-25 10:31:42 -0400

    Added product row

--- src/components/Products.svelte
+++ src/components/Products.svelte
@@ -1,13 +1,23 @@
 <script>
     import ProductCard from "./ProductCard.svelte";
 import SectionWrapper from "./SectionWrapper.svelte";
-    let productFeatures = [
-        {
-            featureList: [],
-            description: "",
-            imgUrl: "",
-        }
-    ];
+let productFeatures = [
+    {
+        featureList: ["Single Speed", "Lightweight Frame", "Reliable Coaster Brakes"],
+        description: "Perfect for casual rides around the island. Our single-speed bikes are easy to handle and great for all ages.",
+        imgUrl: "url_to_single_speed_bike_image.jpg",
+    },
+    {
+        featureList: ["Baby Seat Carrier", "Baby Trailer Attachment", "Sturdy Build"],
+        description: "Experience the ultimate in family-friendly cycling with our versatile bikes. Equipped with a baby seat carrier or a baby trailer attachment, these bikes are built to ensure a comfortable and secure ride for your little ones. ",
+        imgUrl: "url_to_baby_seat_bike_image.jpg",
+    },
+    {
+        featureList: ["Kids Bikes", "Adjustable Seats", "Training Wheels"],
+        description: "Make your child's biking experience fun and memorable. Our kids' bikes come in various sizes and exciting colors.",
+        imgUrl: "url_to_kids_bike_image.jpg",
+    },
+];
 </script>
 
 <SectionWrapper id="products">
@@ -20,12 +30,15 @@ import SectionWrapper from "./SectionWrapper.svelte";
                 Explore our <span class="text-red-400">products</span>
             </h2>
         </div>
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-14 md:gap-14 lg:gap-20 relative text-base">
+        <div class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 relative text-base">
             {#each productFeatures as productFeature, index}
                 <ProductCard index={index} productFeature={productFeature}>
                     {#if index === 0}
+                        Enjoy single speed <span class="text-indigo-400 font-medium">simplicity</span>
                         {:else if index === 1}
+                        <span class="text-indigo-400 font-medium">Family-Friendly </span>rides 
                         {:else}
+                        Bikes for little <span class="text-indigo-400 font-medium">explorers</span>
                     {/if}
                 </ProductCard>
             {/each}
